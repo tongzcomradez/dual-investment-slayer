56efb3df97f78fd36604643a7d0dfcbed610701a
tongzcomrade <s.rotbamrung@gmail.com>
2025-02-10 11:43:50 +0700

    feat: add ma apr to analysis

--- index.ts
+++ index.ts
@@ -7,8 +7,12 @@ import {
   groupAndSortByType,
   groupByDuration,
 } from './utils'
+import { MongoClient } from 'mongodb'
 const _ = require('radash')
 const cron = require('node-cron')
+
+const uri = process.env.MONGO_URI; // เปลี่ยนเป็น URI ของคุณ
+const client = new MongoClient(uri);
 
 const suggestionInvestment = async (command: { chatId: number, threadId?: number, assets: any[] }) => {
   const { assets, chatId, threadId } = command
@@ -24,8 +28,42 @@ const suggestionInvestment = async (command: { chatId: number, threadId?: number
 
   const x = formatOrdersByDuration(_.chain(groupByDuration, groupAndSortByType)(suggestionInvestment))
 
+  await updateMaxApr(suggestionInvestment)
   for (const text of x ) {
     await announce(text,  chatId, threadId)
+  }
+}
+
+const updateMaxApr = async (suggestionInvestment: any[]) => {
+  const flatted = suggestionInvestment.map((item) => Object.values(item)).flat(1) as any[]
+
+  const bulkWrite = flatted.map(f => ({
+    updateOne: {
+      filter: {
+        pairAsset: f.pairAsset,
+        type: f.type,
+        duration: f.duration
+      },
+      update: {
+        $max: {
+          apr: f.apr
+        }
+      },
+      upsert: true
+    }
+  }))
+
+  if (bulkWrite.length === 0) return
+
+  try {
+    await client.connect()
+    console.log("✅ Connected to MongoDB");
+    await client.db("bn-dual-investments").collection("apr").bulkWrite(bulkWrite)
+  }
+  catch(err) {
+    console.error("❌ Error:", err);
+  } finally {
+    await client.close()
   }
 }
 
@@ -36,5 +74,10 @@ const main = async () => {
 };
 
 cron.schedule('*/20 * * * * *', async () => {
-  await main()
+  try {
+    await main()
+  } catch (error) {
+    console.log('error', error)
+  }
 });
+
